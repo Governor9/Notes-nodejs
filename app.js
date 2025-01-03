de1d4345b7d28dd5da149fba5ab393a4b050b94d
Renoir Auguste <renoirsancho@gmail.com>
2025-01-03 14:59:49 +0300

    adding ejs files

--- app.js
+++ app.js
@@ -4,3 +4,25 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
+
+const port = 5000|| process.env.PORT;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//static files
+
+app.use(express.static('public'));
+
+//Templating engine
+app.use(expressLayouts());
+app.set('layout','./layouts/main' );
+app.set('view engine','ejs' );
+
+app.get('/', (req,res)=>{
+    res.render('index');
+});
+
+app.listen(port,()=>{
+  console.log(`App listenimg on port ${port}`);
+});
